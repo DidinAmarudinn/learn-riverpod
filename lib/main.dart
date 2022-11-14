da4e94ca8a5ddff7e7bccb33139a3bb0901c1b27
Didin Amarudin <62782158+DidinAmarudinn@users.noreply.github.com>
2022-11-14 20:29:40 +0700

    learn watch read listen

--- lib/main.dart
+++ lib/main.dart
@@ -55,22 +55,59 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class CounterPage extends StatelessWidget {
+class CounterPage extends ConsumerWidget {
   const CounterPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final int counter = ref.watch(counterProvider);
+
+    ref.listen(
+      counterProvider,
+      (previous, next) {
+        if (next >= 5) {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: const Text('Warning'),
+                content: const Text(
+                    'Counter dangerously high. Consider resetting it.'),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text('OK'),
+                  )
+                ],
+              );
+            },
+          );
+        }
+      },
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Counter"),
+        actions: [
+          IconButton(
+              onPressed: () {
+                ref.invalidate(counterProvider);
+              },
+              icon: const Icon(Icons.refresh))
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          ref.read(counterProvider.notifier).state++;
+        },
         child: const Icon(Icons.add),
       ),
       body: Center(
         child: Text(
-          "0",
+          counter.toString(),
           style: Theme.of(context).textTheme.displayMedium,
         ),
       ),
