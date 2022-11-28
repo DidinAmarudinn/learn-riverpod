39c4586c903ac613315c44f35fe812f5b5fb354c
Didin Amarudin <amardidin53@gmail.com>
2022-11-28 14:27:32 +0700

    add bottom navigation bar

--- lib/reddit_clone/features/home/screens/home_screen.dart
+++ lib/reddit_clone/features/home/screens/home_screen.dart
@@ -1,12 +1,24 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:journal_riverpod/reddit_clone/core/utils.dart';
 import 'package:journal_riverpod/reddit_clone/features/auth/controller/auth_controller.dart';
 import 'package:journal_riverpod/reddit_clone/features/home/delegate/search_community_delegate.dart';
 import 'package:journal_riverpod/reddit_clone/features/home/drawers/community_drawer.dart';
 import 'package:journal_riverpod/reddit_clone/features/home/drawers/profile_drawer.dart';
 
-class HomeScreen extends ConsumerWidget {
+import '../../../theme/theme.dart';
+
+
+class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+  int _page = 0;
 
   void displayDrawer(BuildContext context) {
     Scaffold.of(context).openDrawer();
@@ -16,49 +28,73 @@ class HomeScreen extends ConsumerWidget {
     Scaffold.of(context).openEndDrawer();
   }
 
+  void onPageChanged(int index) {
+    _page = index;
+    setState(() {});
+  }
+
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     final user = ref.watch(userProvider);
+    final currentTheme = ref.watch(themeNotifierProvider);
+    final indexPage = ref.watch(indexPageProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Home"),
-        leading: Builder(
-          builder: (context) {
-            return IconButton(
-              onPressed: () => displayDrawer(context),
-              icon: const Icon(
-                Icons.menu,
-              ),
-            );
-          }
-        ),
+        leading: Builder(builder: (context) {
+          return IconButton(
+            onPressed: () => displayDrawer(context),
+            icon: const Icon(
+              Icons.menu,
+            ),
+          );
+        }),
         actions: [
           IconButton(
             onPressed: () {
-              showSearch(context: context, delegate: SearchCommunityDelegate(ref));
+              showSearch(
+                  context: context, delegate: SearchCommunityDelegate(ref));
             },
             icon: const Icon(
               Icons.search,
             ),
           ),
-          Builder(
-            builder: (context) {
-              return IconButton(
-                onPressed: () => displayEndDrawer(context),
-                icon: CircleAvatar(
-                  backgroundImage: NetworkImage(user?.profilePic ?? ""),
-                ),
-              );
-            }
-          )
+          Builder(builder: (context) {
+            return IconButton(
+              onPressed: () => displayEndDrawer(context),
+              icon: CircleAvatar(
+                backgroundImage: NetworkImage(user?.profilePic ?? ""),
+              ),
+            );
+          })
         ],
         centerTitle: false,
       ),
       drawer: const CommunityDrawer(),
       endDrawer: const ProfileDrawer(),
-      body: Center(
-        child: Text(user?.name ?? ""),
+      bottomNavigationBar: CupertinoTabBar(
+        activeColor: currentTheme.iconTheme.color,
+        currentIndex: indexPage,
+        backgroundColor: currentTheme.backgroundColor,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.add,
+            ),
+          ),
+        ],
+        onTap: (val) {
+          ref.read(indexPageProvider.notifier).state = val;
+        } ,
       ),
+      body: tabWidgets[indexPage]
     );
   }
 }
+
+
